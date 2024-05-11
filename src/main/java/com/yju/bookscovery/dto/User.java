8566f85050a5f5b2d819cf34ce93f86cbaebdbce
gd095529 <gd095529@naver.com>
2024-05-11 15:45:12 +0900

    초기 백엔드 설정파일

---
+++ src/main/java/com/yju/bookscovery/dto/User.java
@@ -0,0 +1,36 @@
+package com.yju.bookscovery.dto;
+
+public class User {
+    private String id;
+    private String pwd;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public User(){}
+    public User(String id, String pwd) {
+        this.id = id;
+        this.pwd = pwd;
+    }
+}
