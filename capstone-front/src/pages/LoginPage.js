3cf5711e4cbb7f0c4ae9376399f33d30f69ec8f7
ch-1029 <cwy0239@naver.com>
2024-05-11 20:11:26 +0900

    capstone

--- capstone-front/src/pages/LoginPage.js
+++ capstone-front/src/pages/LoginPage.js
@@ -1,10 +1,26 @@
 import loginCss from '../styles/Login.module.css';
 import logo from '../images/logo.png';
 import { useState } from 'react';
+import enter from '../images/enter.png';
 
 function LoginPage() {
     const [isFocus, setFocus] = useState(false);
 
+    const checkValue = (e) => {
+        if (e.target.value == 0) {
+            setFocus(false);
+        } else {
+            setFocus(true);
+        }
+    }
+    const pressEnter = (e) => {
+        if (e.key === 'Enter') {
+            checkLogin();
+        }
+    }
+    const checkLogin = () => {
+        return alert('가입되지 않은 정보입니다');
+    }
     return (
         <div className={loginCss.body}>
             <div className={loginCss.loginBox}>
@@ -13,7 +29,9 @@ function LoginPage() {
                     <div>로그인</div>
                 </div>
                 <div className={loginCss.inputBox}>
-                    <input className={isFocus ? loginCss.focusInput : ''}  type='text' placeholder='이메일을 입력하세요.' onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} ></input>
+                    <p style={{opacity: isFocus ?  '0.6' : '0'}}>이메일 입력</p>
+                    <input className={isFocus ? loginCss.focusInput : ''}  type='email' placeholder='이메일을 입력하세요.' onFocus={() => setFocus(true)} onBlur={checkValue} onKeyPress={pressEnter}></input>
+                    <img src={enter} alt='엔터'  style={{opacity: isFocus ? '1' : '0'}} onClick={checkLogin}/>
                 </div>
                 <div className={loginCss.buttonBox}>
                     <div className={loginCss.planeText}>계정이 없으신가요?</div>
