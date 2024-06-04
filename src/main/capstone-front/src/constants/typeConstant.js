bd4688b486c0f04189eb7f4d776f0e2e4ff7c81d
ch-1029 <cwy0239@naver.com>
2024-06-05 04:02:34 +0900

    mainPage 35% success

--- src/main/capstone-front/src/constants/typeConstant.js
+++ src/main/capstone-front/src/constants/typeConstant.js
@@ -1,23 +1,28 @@
-export const type = [
+export const types = [
     {
         name: "도서관 이벤트",
-        type: "event"
+        type: "event",
+        index: 5
     },
     {
         name: "연령별 인기 도서",
-        type: "book"
+        type: "age",
+        index: 6
     },
     {
-        name: "나이별 인기 도서",
-        type: "book"
+        name: "성별 인기 도서",
+        type: "gender",
+        index: 4
     },
     {
         name: "대출 인기 도서",
-        type: "book"
+        type: "loan",
+        index: 5
     },
     {
         name: "학과 인기 도서",
-        type: "book"
+        type: "department",
+        index: 3
     },
 
 ]
