9b052c5bc8718dc3cae0f6ecc04f447062c8555f
parkminuk <parkminuk0203@gmail.com>
2024-06-18 15:06:07 +0900

    아이디 찾기 / 비밀번호 찾기 UI 수정

--- src/main/capstone-front/src/components/FindId.js
+++ src/main/capstone-front/src/components/FindId.js
@@ -1,24 +1,26 @@
 import findCss from "../styles/FindIdPwd.module.css";
 
 function FindId() {
-
-
-
     return (
-        <div>
-            <div>
-                <p>이메일</p>
-                <input type={'email'} />
-            </div>
-            <div>
-                <p>이름</p>
-                <input type={'text'} />
-            </div>
-            <div className={findCss.btn}>
-                <p>아이디 찾기</p>
+        <div className={findCss.body}>
+            <div className={findCss.container}>
+
+                <div className={findCss.card}>
+                    <div className={findCss.field}>
+                        <p>이메일</p>
+                        <input type="email" />
+                    </div>
+                    <div className={findCss.field}>
+                        <p>이름</p>
+                        <input type="text" />
+                    </div>
+                    <div className={findCss.btn}>
+                        <p>아이디 찾기</p>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default FindId;
--- src/main/capstone-front/src/components/FindPwd.js
+++ src/main/capstone-front/src/components/FindPwd.js
@@ -2,20 +2,25 @@ import findCss from "../styles/FindIdPwd.module.css";
 
 function FindPwd() {
     return (
-        <div>
-            <div>
-                <p>이메일</p>
-                <input type={'email'}/>
-            </div>
-            <div>
-                <p>아이디</p>
-                <input type={'text'}/>
-            </div>
-            <div className={findCss.btn}>
-                <p>비밀번호 찾기</p>
+        <div className={findCss.body}>
+            <div className={findCss.container}>
+
+                <div className={findCss.card}>
+                    <div className={findCss.field}>
+                        <p>이메일</p>
+                        <input type="email" />
+                    </div>
+                    <div className={findCss.field}>
+                        <p>아이디</p>
+                        <input type="text" />
+                    </div>
+                    <div className={findCss.btn}>
+                        <p>비밀번호 찾기</p>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default FindPwd;
