216f81182c0e615dc75b3a5ba6010c7036c3bedd
parkminuk <parkminuk0203@gmail.com>
2024-06-18 16:10:35 +0900

    내 정보 수정 / 애니메이션 추가

--- src/main/capstone-front/src/pages/MemberModify.js
+++ src/main/capstone-front/src/pages/MemberModify.js
@@ -1,56 +1,74 @@
 import memberModifyCss from '../styles/MemberModify.module.css';
 import Header1 from "../components/Header1";
-import {departmentList} from "../exportJS/departmentList";
+import { departmentList } from "../exportJS/departmentList";
+import { listsAge, listsGender } from "../constants/exampleListOption";
 
 function MemberModify() {
+    const departments = departmentList(); // 함수 호출하여 배열 반환
+
     return (
         <div className={memberModifyCss.body}>
             <div className={memberModifyCss.header}>
-                <Header1/>
+                <Header1 />
             </div>
-            <div className={memberModifyCss.context}>
-                <div>내 정보 수정</div>
-                <div>
-                    <p>이메일</p>
-                    <input type={'text'} defaultValue={'email@naver.com'} disabled={true}/>
-                </div>
-                <div>
-                    <p>아이디</p>
-                    <input type={'text'} defaultValue={'id'} disabled={true}/>
-                </div>
-                <div>
-                    <p>비밀번호</p>
-                    <input type={'password'} defaultValue={'password'}/>
-                </div>
-                <div>
-                    <p>비밀번호 확인</p>
-                    <input type={'password'} defaultValue={'password'}/>
-                </div>
-                <div>
-                    <p>이름</p>
-                    <input type={'text'} defaultValue={'이름'}/>
-                </div>
-                <div>
-                    <p>성별</p>
-                    <input type={'text'} defaultValue={'남'}/>
-                </div>
-                <div>
-                    <p>연령</p>
-                    <input type={'text'} defaultValue={'20'}/>
-                </div>
-                <div>
-                    <p>학과</p>
-                    <select defaultValue={0}> {/**DB 써서 해당 유저의 과 값 가져올 것.*/}
-                        {
-                            departmentList().map((department, index) => (
-                                <option key={index} value={department.id}>{department.name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div>
-                    <div>수정하기</div>
-                    <div>취소</div>
+            <div className={memberModifyCss.card}>
+                <div className={memberModifyCss.context}>
+                    <div className={memberModifyCss.titleContainer}>
+                        <div className={memberModifyCss.title}>
+                            내 정보 수정
+                            <img
+                                src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmF81H2faUvgPpBs56XmwMliUszrDZ3oCiZQ&s'}
+                                alt="프로필 이미지"
+                                className={memberModifyCss.profileImage}
+                            />
+                        </div>
+                    </div>
+                    <div className={memberModifyCss.field}>
+                        <p className={memberModifyCss.label}>이메일</p>
+                        <input type={'text'} defaultValue={'email@naver.com'} disabled={true} />
+                    </div>
+                    <div className={memberModifyCss.field}>
+                        <p className={memberModifyCss.label}>아이디</p>
+                        <input type={'text'} defaultValue={'id'} disabled={true} />
+                    </div>
+                    <div className={memberModifyCss.field}>
+                        <p className={memberModifyCss.label}>이름</p>
+                        <input type={'text'} />
+                    </div>
+                    <div className={memberModifyCss.field}>
+                        <p className={memberModifyCss.label}>성별</p>
+                        <select defaultValue={0}>
+                            {
+                                listsGender.slice(0, -1).map((gender, index) => (
+                                    <option key={index} value={gender.codeValue}>{gender.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className={memberModifyCss.field}>
+                        <p className={memberModifyCss.label}>연령</p>
+                        <select defaultValue={0}>
+                            {
+                                listsAge.slice(0,-1).map((age, index) => (
+                                    <option key={index} value={age.codeValue}>{age.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className={memberModifyCss.field}>
+                        <p className={memberModifyCss.label}>학과</p>
+                        <select defaultValue={0}>
+                            {
+                                departments.map((department, index) => (
+                                    <option key={index} value={department.id}>{department.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className={memberModifyCss.buttonContainer}>
+                        <div className={memberModifyCss.button}>수정하기</div>
+                        <div className={memberModifyCss.button}>취소</div>
+                    </div>
                 </div>
             </div>
         </div>
