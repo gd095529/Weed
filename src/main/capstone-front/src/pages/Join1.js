004f691fab331455973da8d4e19c7094340f6bdd
parkminuk <parkminuk0203@gmail.com>
2024-06-18 19:14:25 +0900

    회원 가입

--- src/main/capstone-front/src/pages/Join1.js
+++ src/main/capstone-front/src/pages/Join1.js
@@ -1,15 +1,19 @@
 import joinCss1 from '../styles/Join1.module.css';
-import logo from '../images/logo2.png'
-import axios from "axios";
-import {departmentList} from "../exportJS/departmentList";
-import {useNavigate} from "react-router-dom";
-import {useRef, useState} from "react";
+import logo from '../images/logo2.png';
+import axios from 'axios';
+import { departmentList } from '../exportJS/departmentList';
+import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import { TextField, FormControl, InputAdornment, IconButton } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import {listsAge, listsGender} from "../constants/exampleListOption";
 
 function Join1() {
     const navigate = useNavigate();
+
     const onClickLogo = () => {
-        navigate("/");
-    }
+        navigate('/');
+    };
 
     const email = useRef(null);
     const id = useRef(null);
@@ -19,10 +23,6 @@ function Join1() {
     const gender = useRef(null);
     const age = useRef(null);
     const departmentRef = useRef(null);
-
-    const check_password = () => {
-
-    }
 
     async function joinAPI() {
         try {
@@ -40,7 +40,7 @@ function Join1() {
                 }
             });
 
-            navigate("/login1");
+            navigate('/login1');
         } catch (error) {
             console.error(error);
         }
@@ -48,55 +48,106 @@ function Join1() {
 
     return (
         <div className={joinCss1.body}>
-            <div className={joinCss1.head}>
-                <img src={logo} alt={'a'} onClick={onClickLogo}/>
-                <p>회원가입</p>
+            <div className={joinCss1.container}>
+                <div className={joinCss1.head}>
+                    <img src={logo} alt="logo" onClick={onClickLogo} />
+                    <p>회원가입</p>
+                </div>
+                <div className={joinCss1.context}>
+                    <div className={joinCss1.field}>
+                        <FormControl variant="outlined" fullWidth>
+                            <TextField
+                                label="이메일"
+                                type="email"
+                                inputRef={email}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                edge="end"
+                                                className={joinCss1.btnInline}
+                                            >
+                                                <CheckIcon />
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }}
+                            />
+                        </FormControl>
+                    </div>
+                    <div className={joinCss1.field}>
+                        <TextField
+                            label="아이디"
+                            type="text"
+                            inputRef={id}
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </div>
+                    <div className={joinCss1.field}>
+                        <TextField
+                            label="비밀번호"
+                            type="password"
+                            inputRef={password}
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </div>
+                    <div className={joinCss1.field}>
+                        <TextField
+                            label="비밀번호 확인"
+                            type="password"
+                            inputRef={re_password}
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </div>
+                    <div className={joinCss1.field}>
+                        <TextField
+                            label="이름"
+                            type="text"
+                            inputRef={name}
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </div>
+                    <div className={joinCss1.field}>
+                        <p>성별</p>
+                        <select defaultValue={0}>
+                            {
+                                listsGender.slice(0, -1).map((gender, index) => (
+                                    <option key={index} value={gender.codeValue}>{gender.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className={joinCss1.field}>
+                        <p>연령</p>
+                        <select defaultValue={0}>
+                            {
+                                listsAge.slice(0, -1).map((age, index) => (
+                                    <option key={index} value={age.codeValue}>{age.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className={joinCss1.field}>
+                        <p>학과</p>
+                        <select defaultValue={0} ref={departmentRef}>
+                            {
+                                departmentList().map((department, index) => (
+                                    <option key={index} value={department.id}>{department.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className={joinCss1.btnGroup}>
+                        <input type="button" value="회원 가입" className={joinCss1.btn} onClick={joinAPI} />
+                    </div>
+                </div>
             </div>
-            <div className={joinCss1.context}>
-                <div>
-                    <p>이메일</p>
-                    <input type={'email'} ref={email}/>
-                    <input type={'button'} value={'인증'} style={{marginLeft: '1rem'}}/>
-                </div>
-                <div>
-                    <p>아이디</p>
-                    <input type={'text'} ref={id}/>
-                </div>
-                <div>
-                    <p>비밀번호</p>
-                    <input type={'password'} ref={password}/>
-                </div>
-                <div>
-                    <p>비밀번호 확인</p>
-                    <input type={'password'} ref={re_password}/>
-                </div>
-                <div>
-                    <p>이름</p>
-                    <input type={'text'} ref={name}/>
-                </div>
-                <div>
-                    <p>성별</p>
-                    <input type={'text'} ref={gender}/>
-                </div>
-                <div>
-                    <p>연령</p>
-                    <input type={'text'} ref={age}/>
-                </div>
-                <div>
-                    <p>학과</p>
-                    <select defaultValue={0}> {/**DB 써서 해당 유저의 과 값 가져올 것.*/}
-                        {
-                            departmentList().map((department, index) => (
-                                <option key={index} value={department.id} ref={departmentRef}>{department.name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <input type={'button'} value={'회원 가입'} style={{margin: '0 auto', width: '100%'}} onClick={joinAPI}/>
-            </div>
-            <div></div>
         </div>
-    )
+    );
 }
 
 export default Join1;
